8cf7aab8aa8f2559cfdacd2df7d69b4dc9b2c24e
prathmeshml25 <prathmeshatkari09@gmail.com>
2026-08-11 17:38:11 +0530

    Add Python programming lab program

---
+++ Question 17.py
@@ -0,0 +1,7 @@
+# 17. Write a Python program to create a tuple containing a single item, the number 50, and confirm its type. Given Input: A single integer value 50 
+
+value = 50
+single_tuple = (value,)
+
+print(single_tuple)
+print(type(single_tuple))
---
+++ Question 6.py
@@ -0,0 +1,12 @@
+# 6.  Write a Python program to create a set, add a new element to it, remove an element using remove(), and discard an element using discard(). Given Input: fruits = {"apple", "banana", "cherry"} 
+
+fruits = {"apple", "banana", "cherry"}
+
+fruits.add("mango")
+print(fruits)
+
+fruits.remove("banana")
+print(fruits)
+
+fruits.discard("apple")
+print(fruits)
