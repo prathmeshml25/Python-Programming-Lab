517c654fb1d7a4cd562cf8966de7c91696aba357
prathmeshml25 <prathmeshatkari09@gmail.com>
2026-08-11 19:52:56 +0530

    Update Question 16.py

--- Question 16.py
+++ Question 16.py
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
+
 # 16. Write a Python program to create a tuple, access its elements by index, and find its length. Given Input: fruits = ("apple", "banana", "cherry", "date") 
 
 fruits = ("apple", "banana", "cherry", "date")
-
 print(fruits[0])
+print(fruits[1])
 print(fruits[2])
+print(fruits[3])
 print(len(fruits))
-=======
-fruits = ("apple", "banana", "cherry", "date")
-
-print(fruits[0])
-print(fruits[2])
-print(len(fruits))
->>>>>>> 02eb39a094ea1d1be257c35ddc861403c3a8151e
